cf64a4190b94e9293d463328e04468e2c007810c
GOPICHAND REDDY <157137168+Gopichandreddy23@users.noreply.github.com>
2025-09-21 13:22:39 +0530

    Delete Meditech directory

--- Meditech/src/main/java/com/meditech/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.meditech.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.meditech.entity.Product;
-
-public interface ProductRepository extends JpaRepository<Product,Long >{
-
-}
